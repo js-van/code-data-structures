90a3c47e404c63dd7e23c28e5c1a8ec2b6d690c3
Peng Lyu <penn.lv@gmail.com>
2017-12-17 21:27:58 -0800

    add/delete with prefix computer works as expected. It passes the random insert/delete tests.

--- src/piece-table.ts
+++ src/piece-table.ts
@@ -1,45 +1,86 @@
+import { PrefixSumComputer } from './prefixSumComputer';
+import { IPosition, Position } from './position';
+
 interface IPiece {
 	isOriginalBuffer: boolean;
 	offset: number;
 	length: number;
+	
+	lineFeedCnt: number;
+	lineStarts: PrefixSumComputer;
 }
 
-interface IPosition {
+interface BufferCursor {
 	/**
 	 * Piece Index
 	 */
 	index: number;
 	
 	/**
-	 * Character Offset in the particular buffer.
+	 * Character Offset in the particular buffer. 
 	 */
 	offset: number;
+	/** 
+	 * remainer in current piece.
+	*/
+	remainder: number;
+}
+
+interface IRange {
+	/**
+	 * Line number on which the range starts (starts at 1).
+	 */
+	readonly startLineNumber: number;
+	/**
+	 * Column on which the range starts in line `startLineNumber` (starts at 1).
+	 */
+	readonly startColumn: number;
+	/**
+	 * Line number on which the range ends.
+	 */
+	readonly endLineNumber: number;
+	/**
+	 * Column on which the range ends in line `endLineNumber`.
+	 */
+	readonly endColumn: number;
 }
 
 export class PieceTable {
 	private _originalBuffer: string;
 	private _changeBuffer: string;
 	private _pieces: IPiece[];
+	private _lineStarts: PrefixSumComputer; // piece index to line
+	private _indexStarts: PrefixSumComputer; // piece index to offset
 	
 	constructor(originalBuffer: string, size?: number) {
 		this._originalBuffer = originalBuffer;
 		this._changeBuffer = '';
 		
+		const { lineFeedCount, lineLengths } = this.udpateLFCount(originalBuffer);
+		const lineStarts = new PrefixSumComputer(lineLengths);
+		
 		this._pieces = [{
 			isOriginalBuffer: true,
 			offset: 0,
-			length: size ? size : originalBuffer.length
+			length: size ? size : originalBuffer.length,
+			lineFeedCnt: lineFeedCount,
+			lineStarts: lineStarts
 		}];
 	}
 	
 	insert(value: string, offset: number): void {
 		const startOffset = this._changeBuffer.length;
 		this._changeBuffer += value;
-				
+		
+		const { lineFeedCount, lineLengths } = this.udpateLFCount(value);
+		const lineStarts = new PrefixSumComputer(lineLengths);
+
 		const newPiece: IPiece = {
 			isOriginalBuffer: false,
 			offset: startOffset,
-			length: value.length
+			length: value.length,
+			lineFeedCnt: lineFeedCount,
+			lineStarts: lineStarts
 		};
 		
 		// insert newPiece into the piece table.
@@ -47,23 +88,42 @@ export class PieceTable {
 		if (!insertPosition) {
 			throw('this should not happen');
 		}
-		let originalPiece = this._pieces[insertPosition.index]
+		let originalPiece = this._pieces[insertPosition.index];
+		
+		let { index, remainder } = originalPiece.lineStarts.getIndexOf(insertPosition.remainder);
+		let firstPart = {
+			isOriginalBuffer: originalPiece.isOriginalBuffer,
+			offset: originalPiece.offset,
+			length: insertPosition.offset - originalPiece.offset,
+			lineFeedCnt: index,
+			lineStarts: new PrefixSumComputer(originalPiece.lineStarts.values)
+		};
+		
+		firstPart.lineStarts.removeValues(index + 1, originalPiece.lineStarts.values.length - index - 1);
+		firstPart.lineStarts.changeValue(index, remainder);
+		
+		let secondPart = {
+			isOriginalBuffer: originalPiece.isOriginalBuffer,
+			offset: insertPosition.offset,
+			length: originalPiece.length - (insertPosition.offset - originalPiece.offset),
+			lineFeedCnt: originalPiece.lineFeedCnt - index,
+			lineStarts: new PrefixSumComputer(originalPiece.lineStarts.values)
+		}
+		
+		if (index > 0) {
+			secondPart.lineStarts.removeValues(0, index - 1);
+		}
+		
+		secondPart.lineStarts.changeValue(index, originalPiece.offset + originalPiece.length - offset);
+		
 		let newPieces: IPiece[] = [
-			{
-				isOriginalBuffer: originalPiece.isOriginalBuffer,
-				offset: originalPiece.offset,
-				length: insertPosition.offset - originalPiece.offset
-			},
+			firstPart,
 			newPiece,
-			{
-				isOriginalBuffer: originalPiece.isOriginalBuffer,
-				offset: insertPosition.offset,
-				length: originalPiece.length - (insertPosition.offset - originalPiece.offset)
-			}
+			secondPart
 		].filter( piece => {
 			return piece.length > 0;
 		});
-;
+
 		this._pieces.splice(insertPosition.index, 1, ...newPieces);
 	}
 	
@@ -74,12 +134,19 @@ export class PieceTable {
 		if (firstTouchedPiecePos.index === lastTouchedPiecePos.index) {
 			const piece = this._pieces[firstTouchedPiecePos.index];
 			
+			let deleteBegin = piece.lineStarts.getIndexOf(firstTouchedPiecePos.remainder);
+			let deleteEnd = piece.lineStarts.getIndexOf(firstTouchedPiecePos.remainder + cnt);
+			
 			if (firstTouchedPiecePos.offset === piece.offset) {
 				piece.offset += cnt;
 				piece.length -= cnt;
+				piece.lineStarts.changeValue(deleteEnd.index, piece.lineStarts.values[deleteEnd.index] - deleteEnd.remainder);
+				piece.lineStarts.removeValues(0, deleteEnd.index);
 				return;
 			} else if (lastTouchedPiecePos.offset === piece.offset + piece.length) {
 				piece.length -= cnt;
+				piece.lineStarts.removeValues(deleteBegin.index + 1, piece.lineStarts.values.length - deleteBegin.index - 1);
+				piece.lineStarts.changeValue(deleteBegin.index, deleteBegin.remainder);
 				return;
 			}
 		}
@@ -87,17 +154,46 @@ export class PieceTable {
 		const firstTouchedPiece = this._pieces[firstTouchedPiecePos.index];
 		const lastTouchedPiece = this._pieces[lastTouchedPiecePos.index];
 		
-		const newPieces: IPiece[] = [
-			{
+		let newFirstPiece;
+		{
+			let { index, remainder } = firstTouchedPiece.lineStarts.getIndexOf(firstTouchedPiecePos.remainder);
+			newFirstPiece = {
 				isOriginalBuffer: firstTouchedPiece.isOriginalBuffer,
 				offset: firstTouchedPiece.offset,
-				length: firstTouchedPiecePos.offset - firstTouchedPiece.offset
-			},
-			{
+				length: firstTouchedPiecePos.offset - firstTouchedPiece.offset,
+				lineFeedCnt: index,
+				lineStarts: new PrefixSumComputer(firstTouchedPiece.lineStarts.values)
+			};
+		
+			newFirstPiece.lineStarts.removeValues(index + 1, firstTouchedPiece.lineStarts.values.length - index - 1);
+			newFirstPiece.lineStarts.changeValue(index, remainder);
+		}
+		
+		let newLastPiece;
+		{
+			let { index, remainder } = lastTouchedPiece.lineStarts.getIndexOf(lastTouchedPiecePos.remainder);
+			newLastPiece = {
 				isOriginalBuffer: lastTouchedPiece.isOriginalBuffer,
 				offset: lastTouchedPiecePos.offset,
-				length: lastTouchedPiece.length + lastTouchedPiece.offset - lastTouchedPiecePos.offset
+				length: lastTouchedPiece.length + lastTouchedPiece.offset - lastTouchedPiecePos.offset,
+				lineFeedCnt: lastTouchedPiece.lineFeedCnt - index,
+				lineStarts: new PrefixSumComputer(lastTouchedPiece.lineStarts.values)
+			};
+			
+			// todo I doubt whether I should delete `offset`
+			// change value first otherwise the index is wrong.
+			newLastPiece.lineStarts.changeValue(index, newLastPiece.lineStarts.values[index] - remainder/* lastTouchedPiece.offset + lastTouchedPiece.length - offset */);
+			
+			if (index > 0) {
+				newLastPiece.lineStarts.removeValues(0, index);
 			}
+			
+		}
+		
+		const newPieces: IPiece[] = [
+			newFirstPiece,
+			newLastPiece
+			
 		].filter( piece => {
 			return piece.length > 0;
 		});
@@ -105,11 +201,84 @@ export class PieceTable {
 		this._pieces.splice(firstTouchedPiecePos.index, lastTouchedPiecePos.index - firstTouchedPiecePos.index + 1, ...newPieces);
 	}
 	
-	offsetToPieceIndex(offset: number): IPosition {
+	getValueInRange(range: IRange): string {
+		return '';
+	}
+	
+	getLineCount(): number {
+		let cnt = 0;
+		for (let i = 0; i < this._pieces.length; i++) {
+			cnt += this._pieces[i].lineFeedCnt;
+		}
+		
+		return cnt + 1;
+	}
+	
+	getLineContent(lineNumber: number): string {
+		return '';
+	}
+	
+	getOffsetAt(position: IPosition): number {
+		// todo this can definitely be O(logN) with prefix sum, or a tree data structure.
+		let lineNumber = position.lineNumber;
+		let cnt = 0;
+		let index = -1;
+		let leftLen = 0;
+		for (let i = 0; i < this._pieces.length; i++) {
+			cnt += this._pieces[i].lineFeedCnt;
+			if (cnt + 1 >= lineNumber) {
+				index = i;
+				break;
+			} else {
+				leftLen += this._pieces[i].length;
+			}
+		}
+		
+		// TODO, we need to think about lines across pieces.
+		
+		let remainingLine = lineNumber - (cnt - this._pieces[index].lineFeedCnt);
+		// try to get accumulated value of previous line
+		return leftLen + this._pieces[index].lineStarts.getAccumulatedValue(remainingLine - 2) + position.column - 1;
+	}
+
+	getPositionAt(offset: number): Position {
+		// todo this can definitely be O(logN) with prefix sum, or a tree data structure.
+		let remainingOffset = offset;
+		let index = -1;
+		let lfCnt = 0;
+		
+		for (let i = 0; i < this._pieces.length; i++) {
+			if (remainingOffset > this._pieces[i].length) {
+				remainingOffset -= this._pieces[i].length;
+				lfCnt += this._pieces[i].lineFeedCnt;
+			} else {
+				index = i;
+				break;
+			}
+		}
+	
+		let out = this._pieces[index].lineStarts.getIndexOf(remainingOffset);
+		
+		let column = 0;
+		if (out.index === 0) {
+			if (index > 0) {
+				let lineLens = this._pieces[index - 1].lineStarts.values;
+				column += lineLens[lineLens.length - 1];
+			}
+		}
+		
+		// Ensure we return a valid position
+		lfCnt += out.index;
+		return new Position(lfCnt + 1, column + out.remainder + 1);
+	}
+	
+	private offsetToPieceIndex(offset: number, searchStartIndex?: number): BufferCursor {
+		// todo this can be done in O(logN) by prefix sum.
 		if (offset < 0) {
 			return {
 				index: 0,
-				offset: 0
+				offset: 0,
+				remainder: 0
 			};
 		}
 		
@@ -120,7 +289,8 @@ export class PieceTable {
 			if (remainingOffset <= piece.length) {
 				return {
 					index: i,
-					offset: piece.offset + remainingOffset
+					offset: piece.offset + remainingOffset,
+					remainder: remainingOffset
 				};
 			}
 			remainingOffset -= piece.length;
@@ -140,5 +310,28 @@ export class PieceTable {
             }
         });
         return str;
-    };
+	};
+	
+	private udpateLFCount(chunk: string): { lineFeedCount: number, lineLengths: Uint32Array } {
+		let chunkLineFeedCnt = 0;
+		let lastLineFeedIndex = -1;
+		let lineFeedStarts: number[] = [-1];
+
+		while ((lastLineFeedIndex = chunk.indexOf('\n', lastLineFeedIndex + 1)) !== -1) {
+			chunkLineFeedCnt++;
+			lineFeedStarts.push(lastLineFeedIndex);
+		}
+		
+		const lineStartValues = new Uint32Array(chunkLineFeedCnt + 1);
+		for (let i = 1; i <= chunkLineFeedCnt; i++) {
+			lineStartValues[i - 1] = lineFeedStarts[i] - lineFeedStarts[i - 1];
+		}
+		
+		lineStartValues[chunkLineFeedCnt] = chunk.length - lineFeedStarts[lineFeedStarts.length - 1] - 1;
+
+		return {
+			lineFeedCount: chunkLineFeedCnt,
+			lineLengths: lineStartValues
+		};
+	}
 }
--- src/test/piece-table.test.ts
+++ src/test/piece-table.test.ts
@@ -1,11 +1,12 @@
 import { PieceTable } from '../piece-table';
+import { IPosition, Position } from '../position';
 
-
-const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ';
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ \n';
 
 function randomChar() {
 	return alphabet[randomInt(alphabet.length)];
 };
+
 function randomInt(bound: number) {
 	return Math.floor(Math.random() * bound);
 };
@@ -34,7 +35,7 @@ describe('inserts and deletes', () => {
 		expect(pieceTable.getSequence()).toBe('This is a document with some text.This is more text to insert at offset 34.')
 	});
 
-	it('basic inserts', () => {
+	it('more inserts', () => {
 		let pt = new PieceTable('');
 
 		pt.insert('AAA', 0);
@@ -50,7 +51,7 @@ describe('inserts and deletes', () => {
 		expect(pt.getSequence()).toBe('BBBAADDDACCC');
 	});
 
-	it('basic deletes', () => {
+	it('more deletes', () => {
 		let pt = new PieceTable('012345678');
 
 		pt.delete(8, 1);
@@ -93,5 +94,162 @@ describe('inserts and deletes', () => {
 		}
 		
 		expect(pt.getSequence()).toBe(str);
-	})
+	});
+});
+
+describe('prefix sum for line feed', () => {
+	it('basic', () => {
+		let pieceTable = new PieceTable('1\n2\n3\n4');
+		
+		expect(pieceTable.getLineCount()).toBe(4);
+		expect(pieceTable.getPositionAt(0)).toEqual(new Position(1, 1));
+		expect(pieceTable.getPositionAt(1)).toEqual(new Position(1, 2));
+		expect(pieceTable.getPositionAt(2)).toEqual(new Position(2, 1));
+		expect(pieceTable.getPositionAt(3)).toEqual(new Position(2, 2));
+		expect(pieceTable.getPositionAt(4)).toEqual(new Position(3, 1));
+		expect(pieceTable.getPositionAt(5)).toEqual(new Position(3, 2));
+		expect(pieceTable.getPositionAt(6)).toEqual(new Position(4, 1));
+		
+		expect(pieceTable.getOffsetAt({ lineNumber: 1, column: 1 })).toBe(0);
+		expect(pieceTable.getOffsetAt({ lineNumber: 1, column: 2 })).toBe(1);
+		expect(pieceTable.getOffsetAt({ lineNumber: 2, column: 1 })).toBe(2);
+		expect(pieceTable.getOffsetAt({ lineNumber: 2, column: 2 })).toBe(3);
+		expect(pieceTable.getOffsetAt({ lineNumber: 3, column: 1 })).toBe(4);
+		expect(pieceTable.getOffsetAt({ lineNumber: 3, column: 2 })).toBe(5);
+		expect(pieceTable.getOffsetAt({ lineNumber: 4, column: 1 })).toBe(6);
+	});
+	
+	it('append', () => {
+		let pieceTable = new PieceTable('a\nb\nc\nde');
+		pieceTable.insert('fh\ni\njk', 8);
+		
+		expect(pieceTable.getLineCount()).toBe(6);
+		expect(pieceTable.getPositionAt(9)).toEqual(new Position(4, 4));
+		
+		expect(pieceTable.getOffsetAt({ lineNumber: 1, column: 1 })).toBe(0);
+	});
+	
+	it('insert', () => {
+		let pieceTable = new PieceTable('a\nb\nc\nde');
+		pieceTable.insert('fh\ni\njk', 7);
+		
+		expect(pieceTable.getLineCount()).toBe(6);
+		expect(pieceTable.getPositionAt(6)).toEqual(new Position(4, 1));
+		expect(pieceTable.getPositionAt(7)).toEqual(new Position(4, 2));
+		expect(pieceTable.getPositionAt(8)).toEqual(new Position(4, 3));
+		expect(pieceTable.getPositionAt(9)).toEqual(new Position(4, 4));
+		expect(pieceTable.getPositionAt(12)).toEqual(new Position(6, 1));
+		expect(pieceTable.getPositionAt(13)).toEqual(new Position(6, 2));
+		expect(pieceTable.getPositionAt(14)).toEqual(new Position(6, 3));
+		
+		expect(pieceTable.getOffsetAt({ lineNumber: 4, column: 1 })).toBe(6);
+		expect(pieceTable.getOffsetAt({ lineNumber: 4, column: 2 })).toBe(7);
+		expect(pieceTable.getOffsetAt({ lineNumber: 4, column: 3 })).toBe(8);
+		expect(pieceTable.getOffsetAt({ lineNumber: 4, column: 4 })).toBe(9);
+		expect(pieceTable.getOffsetAt({ lineNumber: 6, column: 1 })).toBe(12);
+		expect(pieceTable.getOffsetAt({ lineNumber: 6, column: 2 })).toBe(13);
+		expect(pieceTable.getOffsetAt({ lineNumber: 6, column: 3 })).toBe(14);
+	});
+	
+	it('delete', () => {
+		let pieceTable = new PieceTable('a\nb\nc\ndefh\ni\njk');
+		pieceTable.delete(7, 2);
+		
+		expect(pieceTable.getSequence()).toBe('a\nb\nc\ndh\ni\njk')
+		expect(pieceTable.getLineCount()).toBe(6);
+		expect(pieceTable.getPositionAt(6)).toEqual(new Position(4, 1));
+		expect(pieceTable.getPositionAt(7)).toEqual(new Position(4, 2));
+		expect(pieceTable.getPositionAt(8)).toEqual(new Position(4, 3));
+		expect(pieceTable.getPositionAt(9)).toEqual(new Position(5, 1));
+		expect(pieceTable.getPositionAt(11)).toEqual(new Position(6, 1));
+		expect(pieceTable.getPositionAt(12)).toEqual(new Position(6, 2));
+		expect(pieceTable.getPositionAt(13)).toEqual(new Position(6, 3));
+		
+		expect(pieceTable.getOffsetAt({ lineNumber: 4, column: 1 })).toBe(6);
+		expect(pieceTable.getOffsetAt({ lineNumber: 4, column: 2 })).toBe(7);
+		expect(pieceTable.getOffsetAt({ lineNumber: 4, column: 3 })).toBe(8);
+		expect(pieceTable.getOffsetAt({ lineNumber: 5, column: 1 })).toBe(9);
+		expect(pieceTable.getOffsetAt({ lineNumber: 6, column: 1 })).toBe(11);
+		expect(pieceTable.getOffsetAt({ lineNumber: 6, column: 2 })).toBe(12);
+		expect(pieceTable.getOffsetAt({ lineNumber: 6, column: 3 })).toBe(13);
+	});
+	
+	it('add+delete 1', () => {
+		let pieceTable = new PieceTable('a\nb\nc\nde');
+		pieceTable.insert('fh\ni\njk', 8);
+		pieceTable.delete(7, 2);
+		
+		expect(pieceTable.getSequence()).toBe('a\nb\nc\ndh\ni\njk')
+		expect(pieceTable.getLineCount()).toBe(6);
+		expect(pieceTable.getPositionAt(6)).toEqual(new Position(4, 1));
+		expect(pieceTable.getPositionAt(7)).toEqual(new Position(4, 2));
+		expect(pieceTable.getPositionAt(8)).toEqual(new Position(4, 3));
+		expect(pieceTable.getPositionAt(9)).toEqual(new Position(5, 1));
+		expect(pieceTable.getPositionAt(11)).toEqual(new Position(6, 1));
+		expect(pieceTable.getPositionAt(12)).toEqual(new Position(6, 2));
+		expect(pieceTable.getPositionAt(13)).toEqual(new Position(6, 3));
+		
+		expect(pieceTable.getOffsetAt({ lineNumber: 4, column: 1 })).toBe(6);
+		expect(pieceTable.getOffsetAt({ lineNumber: 4, column: 2 })).toBe(7);
+		expect(pieceTable.getOffsetAt({ lineNumber: 4, column: 3 })).toBe(8);
+		expect(pieceTable.getOffsetAt({ lineNumber: 5, column: 1 })).toBe(9);
+		expect(pieceTable.getOffsetAt({ lineNumber: 6, column: 1 })).toBe(11);
+		expect(pieceTable.getOffsetAt({ lineNumber: 6, column: 2 })).toBe(12);
+		expect(pieceTable.getOffsetAt({ lineNumber: 6, column: 3 })).toBe(13);
+	});
+	
+	it('random insert delete', () => {
+		let str = '';
+		let pt = new PieceTable(str);
+		
+		for (let i = 0; i < 1000; i++) {
+			if (Math.random() < 0.9) {
+				// insert
+				let text = randomStr(100);
+				let pos = randomInt(str.length + 1);
+				pt.insert(text, pos);
+				
+				str = str.substring(0, pos) + text + str.substring(pos);
+			} else {
+				// delete
+				let pos = randomInt(str.length);
+				let length = Math.min(str.length - pos, Math.floor(Math.random() * 10))
+				let deletedText = str.substr(pos, length);
+				pt.delete(pos, length);
+				
+				str = str.substring(0, pos) + str.substring(pos + length);
+			}
+		}
+		expect(pt.getSequence()).toBe(str);
+
+		let lineFeedIndex = -1;
+		let lastLineFeedIndex = -1;
+		let lineCnt = 1;
+		while ((lineFeedIndex = str.indexOf('\r', lineFeedIndex + 1)) !== -1) {
+			if (lineFeedIndex + 1 === str.length) {
+				// last line feed
+				break;
+			}
+			
+			lineCnt += 1;
+			expect(pt.getPositionAt(lineFeedIndex + 1)).toBe(new Position(lineCnt + 1, 1));
+			expect(pt.getOffsetAt(new Position(lineCnt + 1, 1))).toBe(lineFeedIndex + 1);
+		}
+	});
+});
+
+describe('getTextInRange', () => {
+	it('reads substrings from the buffer', () => {
+		let pieceTable = new PieceTable('abc\ndef\nghi');
+		
+		// expect(pieceTable.getValueInRange({ startLineNumber: 1, startColumn: 2, endLineNumber: 1, endColumn: 3})).toBe('b');
+		// expect(pieceTable.getValueInRange({ startLineNumber: 1, startColumn: 2, endLineNumber: 2, endColumn: 3})).toBe('bc\nde');
+		// expect(pieceTable.getValueInRange({ startLineNumber: 1, startColumn: 2, endLineNumber: 3, endColumn: 8})).toBe('bc\ndef\nghi');
+	});
+});
+
+describe('line operations', () => {
+	it('line content, length', () => {
+		let pieceTable = new PieceTable('abc\ndef\nghi');
+	});
 });
